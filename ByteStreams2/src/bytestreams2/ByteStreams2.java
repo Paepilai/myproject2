58f4650256da1861d30c3dd7f5fe53b70416d12e
Paepilai <110712900+Paepilai@users.noreply.github.com>
2023-05-01 15:03:04 +0700

    Add files via upload

---
+++ ByteStreams2/src/bytestreams2/ByteStreams2.java
@@ -0,0 +1,9 @@
+package bytestreams2;
+
+public class ByteStreams2 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ ByteStreams2/src/bytestreams2/Rectangle.java
@@ -0,0 +1,9 @@
+package bytestreams2;
+
+public class Rectangle implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Shape: Rectangle");
+    }
+}
